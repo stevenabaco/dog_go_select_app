202fd494e5fa95ca95e68f87410aeb722aefff11
Steven Abaco <stevenabaco@gmail.com>
2021-05-07 12:17:25 -0500

    Add logic to handle errors with Catch method

--- js/app.js
+++ js/app.js
@@ -7,44 +7,59 @@ const form = document.querySelector('form');
 // ------------------------------------------
 
 function fetchData(url) {
-  return fetch(url)
-  .then(res =>res.json())
+	return fetch(url)
+		.then(checkStatus)
+		.then(res => res.json())
+		.catch(error => console.log('Looks like there was a problem: ', error));
 }
 
-fetchData(`https://dog.ceo/api/breeds/list`)
-  .then(data => generateOptions(data.message));
+fetchData(`https://dog.ceo/api/breeds/list`).then(data =>
+	generateOptions(data.message)
+);
 
-fetchData('https://dog.ceo/api/breeds/image/random')
-  .then(data => generateImage(data.message));
+fetchData('https://dog.ceo/api/breeds/image/random').then(data =>
+	generateImage(data.message)
+);
 // ------------------------------------------
 //  HELPER FUNCTIONS
 // ------------------------------------------
+
+function checkStatus(response) {
+  if (response.ok) {
+    return Promise.resolve(response)
+  } else {
+    return Promise.reject(new Error(response.statusText));
+  }
+}
 function generateOptions(data) {
-  const options = data.map(item => `
+	const options = data
+		.map(
+			item => `
   <option value='${item}'>${item}</option>
-  `).join('');
-  select.innerHTML = options;
+  `
+		)
+		.join('');
+	select.innerHTML = options;
 }
 
 function generateImage(data) {
-  const html = `
+	const html = `
     <img src='${data}' alt>
     <p>Click to view images of ${select.value}s</p>
   `;
-  card.innerHTML = html;
+	card.innerHTML = html;
 }
 
 function fetchBreedImage() {
-  const breed = select.value;
-  const img = card.querySelector('img');
-  const p = card.querySelector('p');
+	const breed = select.value;
+	const img = card.querySelector('img');
+	const p = card.querySelector('p');
 
-  fetchData(`https://dog.ceo/api/breed/${breed}/images/random`)
-    .then(data => {
-      img.src = data.message;
-      img.alt = breed;
-      p.textContent = `Click to view more ${breed}s`
-  })
+	fetchData(`https://dog.ceo/api/breed/${breed}/images/random`).then(data => {
+		img.src = data.message;
+		img.alt = breed;
+		p.textContent = `Click to view more ${breed}s`;
+	});
 }
 // ------------------------------------------
 //  EVENT LISTENERS
